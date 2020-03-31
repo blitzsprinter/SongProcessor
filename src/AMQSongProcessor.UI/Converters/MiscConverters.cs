7215713f865ecc59f551ff204c1b8521485b52d6
AzureSphere <dustin.jason.lee@gmail.com>
2020-03-31 04:49:56 -0700

    Shows in the treeview will not have the expand arrow if all of their songs are hidden.

---
+++ src/AMQSongProcessor.UI/Converters/MiscConverters.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using AMQSongProcessor.Models;
+
+using Avalonia.Data.Converters;
+
+namespace AMQSongProcessor.UI.Converters
+{
+	public static class MiscConverters
+	{
+		public static readonly IValueConverter IgnoredSongs =
+			new FuncValueConverter<IEnumerable<Song>, bool>(x => x.Any(y => !y.ShouldIgnore));
+	}
+}
